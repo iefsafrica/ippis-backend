5857445a58349c9e30e5d9da2fa74548f1f521cf
Taiwo Oladele <taiwodele88@gmail.com>
2026-02-18 19:57:33 +0100

    delete endpoint refactor

--- .next/types/app/api/admin/dashboard/stats/route.ts
+++ .next/types/app/api/admin/dashboard/stats/route.ts
@@ -1,8 +1,8 @@
-// File: C:\Users\PC\ippis-backend\app\api\admin\performance\goal-types\route.ts
-import * as entry from '../../../../../../../app/api/admin/performance/goal-types/route.js'
+// File: C:\Users\PC\ippis-backend\app\api\admin\dashboard\stats\route.ts
+import * as entry from '../../../../../../../app/api/admin/dashboard/stats/route.js'
 import type { NextRequest } from 'next/server.js'
 
-type TEntry = typeof import('../../../../../../../app/api/admin/performance/goal-types/route.js')
+type TEntry = typeof import('../../../../../../../app/api/admin/dashboard/stats/route.js')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
